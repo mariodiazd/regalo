623d9c9a3fc7d8ea31fb209e8e696a3a663347bf
DESKTOP-EC2K65P\mario <mariodiazdiestro@gmail.com>
2022-04-11 00:09:03 +0200

    new

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/felicidades-paula'));
+app.use(express.static('./dist/project-p'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/felicidades-paula/'}),
+    res.sendFile('index.html', {root: 'dist/project-p/'}),
 );
 
 // Start the app by listening on the default Heroku port
